73efc5d36fed02ecd01cf9d37fd9f3dc93b6fe39
carbancle <carbancle2@gmail.com>
2024-03-11 04:18:04 +0900

    0.0.21 axios 파일 수정

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: import.meta.env.PROD
-    ? ""
+    ? "https://car-learn.vercel.app/"
     : `http://localhost:${import.meta.env.VITE_PORT}`,
 });
 
